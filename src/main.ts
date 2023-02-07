80aac4fa73596bffaeb29eaa3ad866893380c418
Kareem Mahlees <kareemmahlees@gmail.com>
2023-02-07 09:12:25 +0200

    add cors, change port to 8000

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
-    await app.listen(3000);
+    app.enableCors({ origin: 'http://localhost:3000', methods: '*' });
+    app.use(helmet());
+    await app.listen(8000);
 }
 bootstrap();
