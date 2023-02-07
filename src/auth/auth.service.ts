178230ebbb11ec1ff1a3d9eb78028eb8c4d5ab38
Kareem Mahlees <kareemmahlees@gmail.com>
2023-02-07 20:05:51 +0200

    add service to prevoius commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
     constructor(private readonly supabase: SupabaseService) {}
 
     async signUpUser(payload: UserSignUpDTO, file: Express.Multer.File) {
-        const { data, error } = await this.supabase.auth.signUp({
+        const { data, error: err1 } = await this.supabase.auth.signUp({
             email: payload.email,
             password: payload.password,
             phone: payload.phone,
@@ -24,15 +24,35 @@ export class AuthService {
                 },
             },
         });
-        if (error != null) {
-            throw new InternalServerErrorException(error.message);
+        if (err1 != null) {
+            throw new InternalServerErrorException(err1.message);
         }
         if (data.user.identities.length === 0) {
             throw new BadRequestException({ error: 'User already signed in' });
         }
-        await this.supabase.storage
+        const { data: uploadedAvatar, error: err2 } =
+            await this.supabase.storage
+                .from('avatars')
+                .upload(`${data.user.id}.png`, file.buffer);
+
+        if (err2 != null) {
+            throw new InternalServerErrorException(err2.name, {
+                description: err2.message,
+            });
+        }
+
+        const { data: avatarUrl, error: err3 } = await this.supabase.storage
             .from('avatars')
-            .upload(`${data.user.id}.png`, file.buffer);
+            .createSignedUrl(uploadedAvatar.path, 3600);
+
+        if (err3 != null) {
+            throw new InternalServerErrorException(err3.name, {
+                description: err3.message,
+            });
+        }
+
+        data.user['avatar_url'] = avatarUrl.signedUrl;
+
         return data.user;
     }
 
